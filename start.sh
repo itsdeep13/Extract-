33e048227bad3f4791741a5d45b1c12899044486
yassuhacks750 <yassudj8085231272@gmail.com>
2025-10-06 23:08:16 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 echo "Cloning Repo..."
 git clone https://github.com/yassuhacks750/Extract-
-cd /Extract-
+cd /Extractor
 pip install -r requirements.txt
 echo "Starting Bot..."
 python -m Extractor
