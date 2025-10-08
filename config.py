f817fcc251d2f88a6c53013480a4a91b4bd3d0a5
itsdeep13 <80030947+itsdeep13@users.noreply.github.com>
2025-10-08 19:10:08 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,14 +1,14 @@
 import os
 from os import getenv
 
-API_ID = int(os.environ.get("API_ID", "22849789"))  # Replace "123456" with your actual api_id or use .env
-API_HASH = os.environ.get("API_HASH", "0fc127c6055acd59f00ec6c229e1e3c4")
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "8325258009:AAGY9-XEq1kI0xDhGWkbOHkysiPSvSYenv8")
+API_ID = int(os.environ.get("API_ID", "22484497"))  # Replace "123456" with your actual api_id or use .env
+API_HASH = os.environ.get("API_HASH", "c38cb053916c47a97590c244663cbaef")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "8260828039:AAGZDWSQyHKiTCFxP7Ub64c5IwrCU86IrFk")
 
-OWNER_ID = int(os.environ.get("OWNER_ID", "7296271316"))  # Your Telegram user ID
-SUDO_USERS = list(map(int, os.environ.get("SUDO_USERS", "").split()))  # Space-separated user IDs
+OWNER_ID = int(os.environ.get("OWNER_ID", "6252997817"))  # Your Telegram user ID
+SUDO_USERS = list(map(int, os.environ.get("SUDO_USERS"6252997817, "").split()))  # Space-separated user IDs
 
-MONGO_URL = os.environ.get("MONGO_URL", "mongodb+srv://yashxug:Yash2835P@yashxug.om3yk1q.mongodb.net/retryWrites=true&w=majority&appName=Yashxug")##your mongo url eg: withmongodb+srv://xxxxxxx:xxxxxxx@clusterX.xxxx.mongodb.net/?retryWrites=true&w=majority
-CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1003093930004"))  # Telegram channel ID (with -100 prefix)
+MONGO_URL = os.environ.get("MONGO_URL", "mongodb+srv://uploaderbot:uploaderbot@cluster0.mpesxpw.mongodb.net/?retryWrites=true&w=majority"
+CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1003120656587"))  # Telegram channel ID (with -100 prefix)
 
-PREMIUM_LOGS = os.environ.get("PREMIUM_LOGS", "-1002947231622")  # Optional here you'll get all logs
+PREMIUM_LOGS = os.environ.get("PREMIUM_LOGS", "-1002934946350")  # Optional here you'll get all logs
